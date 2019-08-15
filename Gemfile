73d72ca30f2021c8cc114f2363f85e1233c53059
Alice Deveaux <alice.dvx@icloud.com>
2019-08-15 17:34:21 +0200

    finished back

--- Gemfile
+++ Gemfile
@@ -28,10 +28,12 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+# gem 'font-awesome'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
-
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
@@ -51,7 +53,6 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
-
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "rootbeer")
+Ingredient.create(name: "mezcal")
+Ingredient.create(name: "absinthe")
+Ingredient.create(name: "whiskey")
+Ingredient.create(name: "olives")
+Ingredient.create(name: "martini")
+Ingredient.create(name: "gin")
+
+Cocktail.create!(name: "Lulu White")
+Cocktail.create!(name: "Dark & Stormy")
+Cocktail.create!(name: "Dirty Martini")
