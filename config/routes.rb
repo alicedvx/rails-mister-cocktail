75e3c9a0aeeef5a987aca522fd1f87b14dff9eac
Alice Deveaux <alice.dvx@icloud.com>
2019-08-16 12:54:22 +0200

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create, :show]
   end
   resources :doses, only: [:destroy]
+
+  root to: 'cocktails#index'
 end
+
+# fake comment
